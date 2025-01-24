d97540a8cfc023360f9c9054c2f285aac2ee1f10
DuanZhipeng <duanzhipeng1997@163.com>
2025-01-24 11:55:42 +0800

    stat map

---
+++ app/api/stat/route.ts
@@ -0,0 +1,94 @@
+import { Db } from 'mongodb';
+import { default as exec_mongo } from '@/app/api/mongo'
+
+
+function getTimestampRange() {
+    // 获取当前日期的午夜时间（24时）
+    const now = new Date();
+    const endOfDay = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, 0, 0, 0);
+
+    // 向前推七天
+    const startOfDay = new Date(endOfDay.getTime() - 10 * 24 * 60 * 60 * 1000);
+
+    // 返回时间戳数组
+    return [startOfDay.getTime(), endOfDay.getTime()];
+}
+
+function getStartOfDay(timestamp: number) {
+    // 将毫秒时间戳转换为Date对象
+    const date = new Date(timestamp);
+
+    // 设置时间为当天的零点
+    date.setHours(0, 0, 0, 0);
+
+    // 返回零点时间的时间戳
+    return date.getTime();
+}
+
+function formatDate(timestamp: number) {
+    const date = new Date(timestamp);
+
+    const yyyy = date.getFullYear(); // 获取年份
+    const mm = String(date.getMonth() + 1).padStart(2, '0'); // 获取月份，+1是因为月份从0开始
+    const dd = String(date.getDate()).padStart(2, '0'); // 获取日期
+
+    return `${yyyy}-${mm}-${dd}`;
+}
+
+export async function GET(request: Request) {
+    const time_range = getTimestampRange();
+    const result: { date: string; temp: number; normal: number; vip: number; }[] = []
+    await exec_mongo(async (unlocker_db: Db) => {
+        const cdk_collection = unlocker_db?.collection("Cdk");
+        const cdk_cursor = cdk_collection?.find({
+            create_time: {
+                $gte: time_range[0], // 大于或等于起始时间
+                $lt: time_range[1]     // 小于结束时间
+            },
+            machine_code: {
+                $ne: "",
+            }
+        }, {
+            projection: {
+                "_id": 1,
+                "cdk_type": 1,
+                "create_time": 1
+            }
+        });
+        const cdk_list = await cdk_cursor.toArray()
+        const temp_map: { [key: number]: [number, number, number] } = {};
+        for (let cdk of cdk_list) {
+            const day_key = getStartOfDay(cdk.create_time);
+            const cdk_type = cdk.cdk_type;
+            if (!(day_key in temp_map)) {
+                temp_map[day_key] = [0, 0, 0];
+            }
+            if (cdk_type == "temp") {
+                temp_map[day_key][0] = temp_map[day_key][0] + 1;
+            } else if (cdk_type == "normal") {
+                temp_map[day_key][1] = temp_map[day_key][1] + 1;
+            } else {
+                temp_map[day_key][2] = temp_map[day_key][2] + 1;
+            }
+            
+        }
+        // 排序
+        const temp_list: [number, [number, number, number]][] = []
+        for (let k of Object.keys(temp_map)) {
+            const key = parseInt(k);
+            const v = temp_map[key];
+            temp_list.push([key, v])
+        }
+        temp_list.sort(i => i[0]);
+        for (let item of temp_list) {
+            const date_str = formatDate(item[0]);
+            result.push({
+                date: date_str,
+                temp: item[1][0],
+                normal: item[1][1],
+                vip: item[1][2],
+            });
+        }
+    });
+    return Response.json({ data: result }); 
+}
---
+++ app/stat/page.tsx
@@ -0,0 +1,121 @@
+"use client"
+import { TrendingUp } from "lucide-react"
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import {
+    ChartConfig,
+    ChartContainer,
+    ChartTooltip,
+    ChartTooltipContent,
+} from "@/components/ui/chart"
+import React, { useEffect, useState } from "react"
+// const chartData = [
+//     { month: "January", desktop: 186, mobile: 80 },
+//     { month: "February", desktop: 305, mobile: 200 },
+//     { month: "March", desktop: 237, mobile: 120 },
+//     { month: "April", desktop: 73, mobile: 190 },
+//     { month: "May", desktop: 209, mobile: 130 },
+//     { month: "June", desktop: 214, mobile: 140 },
+// ]
+const chartConfig = {
+    temp: {
+        label: "Temp",
+        color: "hsl(var(--chart-1))",
+    },
+    normal: {
+        label: "Normal",
+        color: "hsl(var(--chart-2))",
+    },
+    vip: {
+        label: "Vip",
+        color: "hsl(var(--chart-3))",
+    },
+} satisfies ChartConfig
+
+export default function Stat () {
+    const [chartData, setChartData] = useState([])
+    useEffect(() => {
+        fetch("/api/stat")
+        .then(res => res.json())
+        .then(data => data.data)
+        .then(setChartData)
+    }, []);
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <Card className="w-[60vw]">
+                    <CardHeader>
+                        <CardTitle>CDK - 趋势图</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <ChartContainer config={chartConfig}>
+                            <AreaChart
+                                accessibilityLayer
+                                data={chartData}
+                                margin={{
+                                    left: 12,
+                                    right: 12,
+                                }}
+                            >
+                                <CartesianGrid vertical={false} />
+                                <XAxis
+                                    dataKey="date"
+                                    tickLine={true}
+                                    axisLine={true}
+                                    tickMargin={8}
+                                />
+                                <ChartTooltip
+                                    cursor={false}
+                                    content={<ChartTooltipContent indicator="dot" />}
+                                />
+                                <Area
+                                    dataKey="temp"
+                                    type="natural"
+                                    fill="var(--color-temp)"
+                                    fillOpacity={0.4}
+                                    stroke="var(--color-temp)"
+                                    stackId="a"
+                                />
+                                <Area
+                                    dataKey="normal"
+                                    type="natural"
+                                    fill="var(--color-normal)"
+                                    fillOpacity={0.4}
+                                    stroke="var(--color-normal)"
+                                    stackId="a"
+                                />
+                                <Area
+                                    dataKey="vip"
+                                    type="natural"
+                                    fill="var(--color-vip)"
+                                    fillOpacity={0.4}
+                                    stroke="var(--color-vip)"
+                                    stackId="a"
+                                />
+                            </AreaChart>
+                        </ChartContainer>
+                    </CardContent>
+                    <CardFooter>
+                        <div className="flex w-full items-start gap-2 text-sm">
+                            <div className="grid gap-2">
+                                <div className="flex items-center gap-2 font-medium leading-none">
+                                    CDK生成数据 <TrendingUp className="h-4 w-4" />
+                                </div>
+                                <div className="flex items-center gap-2 leading-none text-muted-foreground">
+                                    近一周
+                                </div>
+                            </div>
+                        </div>
+                    </CardFooter>
+                </Card>
+            </main>
+        </div>
+    )
+}
