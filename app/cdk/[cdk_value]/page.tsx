b5bcb4433d2115a211e34211b9fa083f4b050534
DuanZhipeng <duanzhipeng1997@163.com>
2025-01-30 20:31:43 +0800

    dynamic

--- app/cdk/[cdk_value]/page.tsx
+++ app/cdk/[cdk_value]/page.tsx
@@ -1,13 +1,12 @@
 'use client'
-
-import ReactJsonView from '@microlink/react-json-view'
+// import ReactJsonView from '@microlink/react-json-view'
 import { useEffect, useState } from 'react';
-
-
+import dynamic from "next/dynamic"
 
 export default function CdkDetail({ params }: { params: { cdk_value: string } }) {
 
-    const [jsonData, setJsonData] = useState({});
+    const [jsonData, setJsonData] = useState(null);
+    const ReactJsonView = dynamic(() => import('@microlink/react-json-view'))
 
     useEffect(() => {
         fetch(`/api/cdk/${params.cdk_value}`)
@@ -19,7 +18,7 @@ export default function CdkDetail({ params }: { params: { cdk_value: string } })
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <ReactJsonView src={jsonData} />
+                {!!jsonData ? (<ReactJsonView src={jsonData} />): (<p>Loading...</p>)}
             </main>
         </div>
     );
