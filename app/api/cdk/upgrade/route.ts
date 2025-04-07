907ca93d162fde08e0cbaeca0a21c93b487ee394
DuanZhipeng <duanzhipeng1997@163.com>
2025-04-07 22:34:55 +0800

    upgrade

---
+++ app/api/cdk/upgrade/route.ts
@@ -0,0 +1,96 @@
+import { Db } from 'mongodb';
+import { default as exec_mongo } from '@/app/api/mongo'
+import { verify_and_get_name, get_score } from "@/app/api/config"
+import { insert_log, get_partner_score, change_partner_score } from '@/app/api/pgsql';
+import { NextRequest } from 'next/server';
+import { sendMail } from "@/app/api/mailer"
+
+
+function getTimestampAfterNDays(n: number) {
+  // 获取当前时间戳（以毫秒为单位）
+  const now = Date.now();
+  // 将天数转换为毫秒（1天 = 24小时 * 60分钟 * 60秒 * 1000毫秒）
+  const oneDay = 24 * 60 * 60 * 1000;
+  // 计算n天后的时间戳
+  const timestampAfterNDays = now + n * oneDay;
+  return timestampAfterNDays;
+}
+
+
+type UpgradeCdkRequest = {
+  cdk_type: string,
+  password: string,
+  value: string,
+}
+
+export async function POST(request: NextRequest) {
+  const json_body: UpgradeCdkRequest = await request.json();
+  const cdk_type = json_body["cdk_type"];
+  const passowrd = json_body["password"];
+  const value = json_body["value"];
+  const creator = verify_and_get_name(passowrd)
+  if (!!!creator) {
+    return Response.json({ data: ["fuck you asshole!"] });
+  }
+  // 获取升级的目标的积分
+  const score: number = get_score(cdk_type);
+  // 获取该cdk的积分
+  const result = await exec_mongo(async (unlocker_db: Db) => {
+    const cdk_collection = unlocker_db?.collection("Cdk");
+    const cdk = await cdk_collection?.findOne({
+      "value": value
+    });
+    if (!!!cdk) {
+      return { data: ["CDK不对哈！"]}
+    }
+    const cdk_creator = cdk.creator;
+    const cdk_score = cdk.score || 0;
+    if (cdk_score >= score) {
+      return { data: ["向下升级？CDK无升级必要"]}
+    }
+    // 获取管理人积分
+    const partner_score = await get_partner_score(cdk_creator);
+    // 计算要扣除的差额
+    const score_diff = score - cdk_score;
+    if (score_diff > partner_score) {
+      return { data: [`积分不足, 需扣除: ${score_diff}, 当前剩余: ${partner_score}`]};
+    }
+
+    const update_data:Record<string, string | number| object> = {};
+    // 修改cdk
+    if (cdk_type == "10years") {
+      update_data["$set"] = {
+        "expire_time": getTimestampAfterNDays(365*10),
+        "cdk_type": "normal",
+        "type_tag": cdk_type,
+        "score": score
+      };
+    } else if (cdk_type == "vip") {
+      update_data["$set"] = {
+        "cdk_type": "vip",
+        "type_tag": cdk_type,
+        "score": score
+      };
+      update_data["$unset"] = {
+        "expire_time": "",
+      };
+    } else if (cdk_type == "yearly") {
+      update_data["$set"] = {
+        "expire_time": getTimestampAfterNDays(365),
+        "cdk_type": "normal",
+        "type_tag": cdk_type,
+        "score": score
+      };
+    }
+    const balance_left = partner_score - score_diff;
+
+    await insert_log({ oper_name: creator, oper_time: new Date(), cdk_value: value, oper_type: "UPGRADE", balance: balance_left });
+    await change_partner_score(creator, -1 * score_diff);
+    await cdk_collection.updateOne({"_id": cdk._id}, update_data);
+    const result = await sendMail(process.env.MAIL_NOTIFY_EMIAL as string, `[CREATE KEY]CDK已升级-${cdk_creator}`, `${value}  |  升级为${cdk_type}  |  ${balance_left}`);
+    console.log(result);
+    return { data: `${value}升级完成`, score_charge: score_diff, partner_score: balance_left }
+  });
+
+  return Response.json(result);
+}
---
+++ app/cdk/upgrade/page.tsx
@@ -0,0 +1,167 @@
+'use client'
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import { Input } from "@/components/ui/input"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
+import { Terminal } from "lucide-react"
+import { useState } from "react";
+import { useToast } from "@/hooks/use-toast"
+
+
+const formSchema = z.object({
+    password: z.string().min(1, {
+        message: "请输入密码"
+    }),
+    value: z.string(),
+    cdk_type: z.string(),
+})
+
+export default function Home() {
+    const { toast } = useToast()
+    const [cdk_value, set_cdk_value] = useState([])
+    const [balance_left, set_balance_left] = useState(-1)
+    const [score_charge, set_score_charge] = useState(-1)
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            password: "",
+            cdk_type: "vip",
+            value: "",
+        },
+    })
+
+    // 2. Define a submit handler.
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        // console.log(values)
+        fetch('/api/cdk/upgrade', {
+            method: 'POST', // 或者 'PUT'
+            headers: {
+                'Content-Type': 'application/json' // 指定发送的数据类型为JSON
+            },
+            body: JSON.stringify(values) // 将JavaScript对象转换为JSON字符串
+        })
+            .then(response => {
+                // 检查响应状态
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json(); // 解析JSON数据
+            })
+            .then(response => {
+                // 处理返回的JSON数据
+                const { data, partner_score = -1, score_charge = -1 } = response;
+                set_cdk_value(data);
+                set_balance_left(partner_score);
+                set_score_charge(score_charge);
+                toast({
+                    title: "提示",
+                    description: "升级成功",
+                    duration: 1000,
+                });
+            })
+            .catch(error => {
+                // 处理错误
+                console.error('There has been a problem with your fetch operation:', error);
+            });
+    }
+
+
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+                        <FormField
+                            control={form.control}
+                            name="password"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>密码</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="xxx" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        预置密码
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="value"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>CDK</FormLabel>
+                                    <FormControl>
+                                        <Input placeholder="XXXX-XXXX-XXXX-XXXX" {...field} />
+                                    </FormControl>
+                                    <FormDescription>
+                                        CDK
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <FormField
+                            control={form.control}
+                            name="cdk_type"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>升级类型</FormLabel>
+                                    <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                        <FormControl>
+                                            <SelectTrigger>
+                                                <SelectValue placeholder="选择生成类型" />
+                                            </SelectTrigger>
+                                        </FormControl>
+                                        <SelectContent>
+                                            <SelectItem value="yearly">年卡（无限制）</SelectItem>
+                                            <SelectItem value="10years">十年（无限制）</SelectItem>
+                                            <SelectItem value="vip">VIP（永久）</SelectItem>
+                                        </SelectContent>
+                                    </Select>
+                                    <FormDescription>
+                                        选择升级CDK的类型
+                                    </FormDescription>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                        <Button type="submit">生成</Button>
+                    </form>
+                </Form>
+                {!!cdk_value && cdk_value.length > 0 && (<Alert>
+                    <Terminal className="h-4 w-4" />
+                    <AlertTitle>升级完成</AlertTitle>
+                    <AlertDescription>
+                        {score_charge != -1 && (<p>消费积分：{score_charge}</p>)}
+                        {balance_left != -1 && (<p>剩余积分：{balance_left}</p>)}
+                    </AlertDescription>
+                </Alert>)}
+
+            </main>
+        </div>
+    );
+}
