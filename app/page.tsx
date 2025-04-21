8da19471c5495b8f089bbf4c7cb842b5464f88fc
DuanZhipeng <duanzhipeng1997@163.com>
2025-04-21 18:07:37 +0800

    new emoji

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ export default function Home() {
                     <SelectContent>
                       <SelectItem value="1_normal">【✅不清除】1次卡（30天有效期）</SelectItem>
                       <SelectItem value="2_normal">【✅不清除】2次卡（30天有效期）</SelectItem>
-                      <SelectItem value="3_normal">【✅不清除】3次卡（3天有效期）</SelectItem>
+                      <SelectItem value="3_normal">【✅不清除】🎁3次卡（3天有效期）</SelectItem>
                       <SelectItem value="3_trial">【❌清除】3次卡（3天有效期）</SelectItem>
                       <SelectItem value="5_trial">【❌清除】5次卡（7天有效期）</SelectItem>
                       <SelectItem value="monthly">【✅不清除】无限次（月卡）</SelectItem>
